99c2f6b05eb79a01ecc946ee0bae0ca9d66c28a6
Nasir Nooruddin <nasir@nasir.id>
2023-11-30 09:59:35 +0700

    first upload

--- microservices/src/orders/server.js
+++ microservices/src/orders/server.js
@@ -1,3 +1,4 @@
+/*
 Copyright 2019 Google LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,3 +12,26 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+*/
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 8081;
+
+//Load orders for pseudo database
+const orders = require("./data/orders.json").orders;
+
+//Enable cors
+app.use(cors());
+
+//Get all orders
+app.get("/api/orders", (req, res) => res.json(orders));
+
+//Get orders by ID
+app.get("/api/orders/:id", (req, res) =>
+  res.json(orders.find(order => order.id === req.params.id))
+);
+
+app.listen(port, () =>
+  console.log(`Orders microservice listening on port ${port}!`)
+);
